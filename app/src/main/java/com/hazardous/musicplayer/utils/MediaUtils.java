3de5227cfa61ca9e05d7ba73868fafe1ba059f1f
Nathanael Ama <nathanael.ama1@gmail.com>
2015-07-06 17:05:59 +0200

    Trying some things found on Stack Overflow, moving call receiving and other music intents to one class

--- app/src/main/java/com/hazardous/musicplayer/utils/MediaUtils.java
+++ app/src/main/java/com/hazardous/musicplayer/utils/MediaUtils.java
@@ -2,19 +2,22 @@ package com.hazardous.musicplayer.utils;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.view.View;
 
 /**
  * Created by Nathanael Ama on 6/27/2015.
  */
 public class MediaUtils extends Activity {
 
-    public void getMusic(View view) {
-//        Context context = getActivity();
-        ContentResolver contentResolver = getContentResolver();
-        Uri allMusic = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+    Context context;
+    ContentResolver contentResolver;
+    Uri allMusic;
+
+    public void getMusic() {
+        contentResolver = getContentResolver();
+        allMusic = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
     }
 
 
